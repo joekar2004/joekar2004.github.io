1221a34335f280c1c76a14b51b446285d1a3449b
joekar2004 <91312553+joekar2004@users.noreply.github.com>
2021-10-01 09:53:22 +0200

    helo

---
+++ vigScript.js
@@ -0,0 +1,22 @@
+function encode() {
+    var txt = document.getElementById("cleartext").value;
+    var shift2 = document.getElementById("shift").value;
+    var shift = {}
+    for (var i = 0; i < shift.length; i++) {
+        shift[i] = shift2[i];
+    }
+    console.log(shift);
+    if (document.getElementById("decision").selectedIndex == 0) {
+        document.getElementById("output").innerHTML = vigDcode(txt, shift);
+    } else {
+        document.getElementById("output").innerHTML = vigEcode(txt, shift);
+    }
+}
+
+function vigDcode(decoded, shift) {
+    
+}
+
+function vigEcode(encoded, shift) {
+
+}
